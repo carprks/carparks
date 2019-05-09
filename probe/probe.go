7056613e0a80d1f44c75cb02a247ac8a45d4f6c2
Max Hooton <max.hooton@auden.co.uk>
2019-05-09 11:11:54 +0100

    inline with the other probes

--- probe/probe.go
+++ probe/probe.go
@@ -2,27 +2,34 @@ package probe
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-// Probe responds with a healthy model
-func Probe(w http.ResponseWriter, r *http.Request) {
+// HTTP responds with a healthy model
+func HTTP(w http.ResponseWriter, r *http.Request) {
 	buf, _ := ioutil.ReadAll(r.Body)
 	if len(buf) >= 1 {
 		log.Println("Probe Request", string(buf))
 	}
 
+	// get probe response
+	resp, _ := Probe()
+
 	// send status
-	j, _ := json.Marshal(Healthy{
-		Status: "pass",
-	})
+	j, _ := json.Marshal(resp)
 	w.Header().Set("Content-Type", "application/health+json")
 	w.Header().Set("Service", "CarParks")
 	w.WriteHeader(http.StatusOK)
 	w.Write(j)
 
 	return
+}
+
+// Probe responds with a healthy model
+func Probe() (Healthy, error){
+	return Healthy{
+		Status: "pass",
+	}, nil
 }
